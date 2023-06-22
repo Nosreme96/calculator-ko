eaad5a424a03c985c0d7e1079b7c82a1468611f5
Nosreme96 <135352945+Nosreme96@users.noreply.github.com>
2023-06-22 13:20:56 +0800

    fixed some logic

--- script-calc.js
+++ script-calc.js
@@ -15,36 +15,46 @@ function clear(){
     var1.length = 0;
     var2.length = 0;
     operand.length = 0;
+    dot = 0;
 }
 
-strRegex = new RegExp(/^[0-9]+$/i);
+strRegex = new RegExp(/^[0-9.]+$/i);
+
+
 
 
 const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', function(){
-    if(strRegex.test(button.innerText) && operand.length == 0 && dot==0)
+buttons.forEach(button => button.addEventListener('click', function uiButton(){
+    if(strRegex.test(button.innerText) && operand.length == 0)
     {      
-         var1.push(button.innerText); 
-         if(button.innerText== ".") {
+        if(button.innerText == "." && !var1.includes('.') && dot == 0 ) {
+            var1.push(button.innerText); 
             dot = 1;
          } 
+         else if(button.innerText != ".")
+         {
+            var1.push(button.innerText); 
+         }
          document.getElementById('display').innerHTML = var1.join("");
 
     
     }
-    else if(strRegex.test(button.innerText) && operand.length != 0 && dot==0)
+    else if(strRegex.test(button.innerText) && operand.length != 0)
     {
-        var2.push(button.innerText);
-        if(button.innerText== ".") {
+        if(button.innerText== "." && !var2.includes('.') && dot == 0) {
+            var2.push(button.innerText); 
             dot = 1;
          } 
+         else if(button.innerText != ".")
+         {
+            var2.push(button.innerText); 
+         }
          document.getElementById('display').innerHTML = var2.join("");
 
     }
     else if(var1.length != 0 &&  button.className=="op")
     {
-        console.log(button,this.className);
-        if(operand.length==0)
+                if(operand.length==0)
         {
        operand.push(button.innerText);
        return operand[0];
@@ -60,8 +70,8 @@ buttons.forEach(button => button.addEventListener('click', function(){
         console.log(answer);
         console.log(var1, operand, var2);
         clear();
-        var1 = Array.from(answer.toString());
-        document.getElementById('display').innerHTML=answer.toString();
+        var1 = Array.from(answer.toString()).slice(0,9);
+        document.getElementById('display').innerHTML = var1.join("");
         
     }
     else if(button.id == "del")
@@ -77,9 +87,12 @@ buttons.forEach(button => button.addEventListener('click', function(){
         console.log(answer);
         console.log(var1, operand, var2);
         clear();
-        var1 = Array.from(answer.toString());
-        document.getElementById('display').innerHTML=answer.toString();
+        var1 = Array.from(answer.toString()).slice(0,9);
+        document.getElementById('display').innerHTML=var1.join("");
     }
 }));
+window.addEventListener("keydown", function (event) {
+console.log(event.key);});
+
 
 
